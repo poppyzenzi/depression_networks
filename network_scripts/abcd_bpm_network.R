d50e91c3e02dc0907337eab5cb40b565fa12b0fe
Poppy Grimes <pzgrimes1@gmail.com>
2023-10-31 17:08:23 +0000

    bpm network add PRS

--- network_scripts/abcd_bpm_network.R
+++ network_scripts/abcd_bpm_network.R
@@ -5,15 +5,6 @@ library(bootnet)
 
 # split into QC and network scripts 
 
-# node strength = how well directly connected to others
-# node closeness = how well indirectly connected to other nodes
-# betweenness = how important in average path between two others
-
-## pairwise Markov random field (PMRF)
-# binary data = Ising model, multivariate normal density = GGM, 
-# non-nomral conitnuous = requires nonparanomral transformation before estimating GGM)
-# mixed graphical models can estimate PMRF w both cont and cat vars 
-
 '9 I feel worthless or inferior
 11 I am too fearful or anxious	
 12 I feel too guilty	
@@ -45,7 +36,10 @@ bpm_int <- bpm %>%
 # make sum score
 #bpm_int$sumscore <- rowSums(bpm_int[symptoms])
 
-#### symptom frequency table #####
+#####################################
+########## symptom tables ###########
+
+# frequency table
 freq_dat <- bpm_int
 colnames(freq_dat) <- c("id","time", "sum", unlist(labels))
 
@@ -59,61 +53,117 @@ print(frequency_table)
 setwd("/Volumes/igmm/GenScotDepression/users/poppy/abcd/symptom_data")
 write.csv(frequency_table, file="abcd_symptom_frequencies.csv")
 
+# proportion table
+prop_table <- freq_dat %>%
+  gather(symptom, value, `worthless`:`worry`) %>% # gather symptoms into key value pairs 
+  group_by(time, symptom) %>% # group by time
+  summarize(proportion = sum(value == 1) / n()) %>% # calculate proportion of "yes" responses
+  spread(time, proportion, fill = 0)
+
+print(prop_table)
+write.csv(prop_table, file="abcd_symptom_proportions.csv")
+
 ###################################
-####### run bootnet sample ########
+## 1: Estimate network
+###################################
 
-# empty list
-result_list <- list()
+theme = 'Reddit'
 
-# run bootnet loop over sweeps
+# estimate network and loop over sweeps, use ggmModSelect or EBICglasso?
+network_list <- list()
+
 for (wave in c(2,4,6,8)) {
   timepoint <- bpm_int$eventname == wave
   data_subset <- bpm_int[timepoint, 4:ncol(bpm_int)]
-  results <- bootnet(data_subset, nBoots=20, default="ggmModSelect")
-  result_list[[as.character(wave)]] <- results
+  network <- estimateNetwork(data_subset,default="ggmModSelect")
+  network_list[[as.character(wave)]] <- network
 }
 
-# Loop through the results and generate plots
-for (wave in names(result_list)) {
-  results <- result_list[[wave]]
-  plot(results$sample, label = labels)
+# Loop through the results and generate plot for estimated networks
+for (wave in names(network_list)) {
+  results <- network_list[[wave]]
+  qgraph(results$graph, layout='spring', theme=theme, labels = labels)
+  #centralityPlot(results, include = c("Betweenness","Closeness","Strength"), labels=labels)
+  title(paste("Wave =", wave), adj=1.0, line= -1.0) 
+}
+
+######################################
+## 2: Run bootnet resampled network
+#######################################
+
+boot1_result_list <- list()
+
+## for each wave run bootnet and store results in list 
+for (wave in names(network_list)) {
+  results <- network_list[[wave]]
+  boot1<-bootnet(results, nBoots=1000, default="ggmModSelect", nCores=1)
+  boot1_result_list[[as.character(wave)]] <- boot1
+}
+
+## plot bootstrapped network results
+for (wave in names(boot1_result_list)) {
+  results <- boot1_result_list[[wave]]
+  plot(results$sample, label = labels, theme=theme)
+  title(paste("Wave =", wave), adj=1.0, line= -1.0) 
+}
+
+# plot bootstrapped edge CIs
+# Create a list of plots and arrange in 1xn grid
+plot_list <- lapply(boot1_result_list, function(boot_result) {
+  plot(boot_result, labels = FALSE, order = "sample")})
+grid.arrange(grobs = plot_list, ncol = 4)
+
+# bootstrapped difference tests between non-zero edge-weights
+# grey = non sig diff, black = sig diff, coloured = colour of edge
+## plot significant differences (alpha=0.05) of edges: 
+# Create a list of edge difference plots and arrange in 1xn grid
+edge_plot_list <- lapply(boot1_result_list, function(boot_result) {
+  plot(boot_result, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample", labels=FALSE)})
+grid.arrange(grobs = edge_plot_list, ncol = 4)
+
+## Plot significant differences (alpha=0.05) of nodestrength
+# grey = non sig, black = sig, white = value of node strength 
+# Create a list of strength difference plots and arrange in 1xn grid
+strength_plot_list <- lapply(boot1_result_list, function(boot_result) {
+  plot(boot_result, "strength", plot = "difference", order = "sample")})
+grid.arrange(grobs = strength_plot_list, ncol = 4)
+
+# test for difference in strength between two nodes
+s1 = "bpm_19_y"
+s2 = "bpm_12_y"
+differenceTest(boot1, s1, s2, "strength")
+
+##################################################
+################# genetic data ###################
+##################################################
+
+setwd('/Users/poppygrimes/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Edinburgh/prs/prs_bpm_OUT')
+prs <- read.table('t2/abcd_mdd_prs_0320.best', header = TRUE)
+prs <- prs[,c(2,4)]
+# make IDs the same in the symptom data and merge
+bpm_qc <- bpm_int %>% rename('IID'='src_subject_id')
+symp_gen <- merge(bpm_qc, prs, by = 'IID')
+length(unique(symp_gen$IID)) # N=4135 with symptom and genetic data
+
+# normalise PRS to [0,1] ?
+#symp_gen$PRS <- (scale(symp_gen$PRS) - min(scale(symp_gen$PRS))) / (max(scale(symp_gen$PRS)) - min(scale(symp_gen$PRS)))
+
+## mixed graphical model for mixed data types
+prs_network_list <- list()
+
+for (wave in c(2,4,6,8)) {
+  timepoint <- symp_gen$time == wave
+  data_subset <- symp_gen[timepoint, 4:ncol(symp_gen)]
+  prs_network <- estimateNetwork(symp_gen[,4:ncol(symp_gen)], default="EBICglasso")
+  prs_network_list[[as.character(wave)]] <- prs_network
+}
+
+# Loop through the results and generate plot for estimated networks
+for (wave in names(prs_network_list)) {
+  results <- prs_network_list[[wave]]
+  qgraph(results$graph, layout='spring', theme='colorblind', labels=c(unlist(labels),"PRS"))
   title(paste("Wave =", wave), adj=0.8)  # line = -0.1 to lower
 }
-
-# ===============================
-
-
-## steps for assessing accuracy of psychological network structures (Fried tutorial paper)
-# a) estimate CIs on edge weights. Use bootstrapping to repeatedly estimate model under sampled data and estimate statistic of interest.
-# b) assess stability of centrality indices in subsets of cases
-# c) test for sig diff btw edge weights and centrality indices
-
-# symptoms are ordinal: compute polychoric correlation matrix as input using qgraph::cor_auto
-
-
-# estimate network
-Network <- estimateNetwork(bpm_int[timepoint, 4:9],default="EBICglasso")
-
-# plot estimated network
-plot(Network, layout='spring', labels=TRUE)
-# plot centrality indices
-centralityPlot(Network, include = c("Betweenness","Closeness","Strength"))
-
-
-## non-parametric bootstrap (important to use for ordinal data)
-boot1<-bootnet(Network, nBoots=1000, nCores=1)
-
-# plot bootstrapped edgeCIs: indicates care to be taken interpreting edges in network 
-plot(boot1, labels=FALSE, order="sample")
-
-# plot significant differences (alpha=0.05) of edges: 
-plot(boot1, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample")
-# Plot significant differences (alpha=0.05) of nodestrength: 
-plot(boot1,"strength",plot="difference")
-
-# test for difference in strength between node x and y
-differenceTest(boot1,"bpm_19_y","bpm_13_y","strength")
-
 
 
 ####################################
