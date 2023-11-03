b1bcce8c97954ca1784507bc1c2c6308c086d4c1
Poppy Grimes <pzgrimes1@gmail.com>
2023-11-03 11:56:33 +0000

    alspac network temp

---
+++ network_temperature/alspac_temperature.R
@@ -0,0 +1,137 @@
+#########################
+library (foreign)
+library(bootnet)
+library(psychonetrics)
+library(dplyr)
+library (qgraph)
+
+
+#########################
+#Data
+
+setwd('/Volumes/igmm/GenScotDepression/users/poppy/alspac')
+smfq_qcd <- read.table('smfq_symptoms_qcd.txt', check.names = FALSE)
+
+labels <- c("unhappy", "anhedonia", "apathetic", "restless", "worthless",
+            "tearful", "distracted", "self-loathing", "guilty", "isolated", 
+            "unloved", "inadequate", "incompetent")
+
+colnames(smfq_qcd) <- c('id', 'time', unlist(labels))
+
+#recode variables so that each variable is binary with +1 and -1 
+smfq_qcd <- smfq_qcd %>%
+  mutate(across(3:15, ~case_when(
+    . == 1 ~ 1,
+    . == 0 ~ -1,
+    TRUE ~ .
+  )))
+
+### fit psychonetrics model
+
+#fit an Ising model with increasing constraints representing their hypotheses to this longitudinal assessment 
+# We investigate the impact on the fit of the model of 
+# 1) constraining edges between nodes to be equal across time points
+# 2) constraining the external fields to be equal across time points 
+# 3) constraining the temperature (the entropy of the system) to be equal across time points. 
+# Additionally, we tested whether a dense network (all nodes are connected) or 
+#  a sparse network (at least some edges are absent) fits the data best
+
+# Variables to use:
+vars <- names(smfq_qcd)[3:15]
+
+# Form saturated model and run [all params free]
+model1 <- Ising(smfq_qcd, vars = vars, groups = "time")
+model1 <- model1 %>% runmodel
+# Prune-stepup to find a sparse model:
+model1b <- model1 %>% prune(alpha = 0.05) %>%  stepup(alpha = 0.05)
+
+# Equal networks (omega = network structure, edges btw nodes equal across time):
+model2 <- model1 %>% groupequal("omega") %>% runmodel
+# Prune-stepup to find a sparse model:
+model2b <- model2 %>% prune(alpha = 0.05) %>% stepup(mi = "mi_equal", alpha = 0.05)
+
+# Equal thresholds (tau = threshold/intercept structure, omega still equal plus external fields equal):
+model3 <- model2 %>% groupequal("tau") %>% runmodel
+# Prune-stepup to find a sparse model:
+model3b <- model3 %>% prune(alpha = 0.05) %>% stepup(mi = "mi_equal", alpha = 0.05)
+
+# Equal beta (beta = inverse temperature, omega and tau also constrained):
+model4 <- model3 %>% groupequal("beta") %>% runmodel
+# Prune-stepup to find a sparse model:
+model4b <- model4 %>% prune(alpha = 0.05) %>% stepup(mi = "mi_equal", alpha = 0.05)
+
+# Compare all models, increasing constraints:
+# RMSEA <0.05 good, minimise AIC and BIC
+psychonetrics::compare(
+  `1. all parameters free (dense)` = model1, # saturated
+  `2. all parameters free (sparse)` = model1b,
+  `3. equal networks (dense)` = model2, # equal network structure 
+  `4. equal networks (sparse)` = model2b,
+  `5. equal networks and thresholds (dense)` = model3, # structure and external fields
+  `6. equal networks and thresholds (sparse)` = model3b,
+  `7. all parameters equal (dense)` = model4, # structure, external and temperature 
+  `8. all parameters equal (sparse)` = model4b
+) %>% arrange(BIC) 
+
+#extract and plot network
+network_smfq <- getmatrix(model2, "omega")[[1]]
+graph_smfq <- qgraph(network_smfq, layout = 'spring', labels = vars, theme = 'colorblind')
+
+#extract temperature 
+temp_smfq <-  as.numeric(lapply(getmatrix(model2, "beta"), 'mean'))
+
+#extract external fields
+fields_smfq <- lapply(getmatrix(model2, 'tau'), 'mean')
+
+
+### plotting
+
+pdf('alspac_network_temp.pdf', 6, 3)
+layout(matrix(c(1,2,2,2,2,3,3,3,3), 1, 9, byrow = TRUE))
+
+par(mar = rep(1,4))
+plot(NULL ,xaxt='n',yaxt='n',bty='n',ylab='',xlab='', xlim=0:1, ylim=0:1)
+legend('center', c("unhappy", "anhedonia", "apathetic", "restless", "worthless",
+                   "tearful", "distracted", "self-loathing", "guilty", "isolated", 
+                   "unloved", "inadequate", "incompetent"), 
+       title = 'SMFQ items', col = 'darkorange', pch = 19,
+       cex = 0.6, bty = 'n'
+       )
+
+# a label at 3rd margin (top) at 0.1 along and right justification (2)
+mtext('(a)', 3, at = 0.5, padj = 2)
+
+qgraph(network_smfq, layout = 'spring', 
+       groups = list(1:13), palette = 'colorblind',
+       legend = FALSE, theme = 'colorblind',
+       labels = c("unhappy", "anhedonia", "apathetic", "restless", "worthless",
+                  "tearful", "distracted", "self-loathing", "guilty", "isolated", 
+                  "unloved", "inadequate", "incompetent"), vsize = 12)
+
+par(mar = c(4, 3, 4, 2), 
+    cex.main = 0.8)
+
+plot(1/temp_smfq, bty = 'n', xlab = 'Age', ylab = 'Temperature', xaxt = 'n', yaxt = 'n', 
+     #ylim = c(.85, 1), 
+     type = 'b', main = 'Change in network temperature')
+axis(1, c(seq(1, 4, 1)), c('11', '13', '14', '17'))
+axis(2, c(seq(.8, 2, .05)))
+mtext('(b)', 3, at = .32, padj = -2)
+
+dev.off()
+
+par(mar = c(6, 4, 6, 2))
+
+hist(all9296$ft1992, main = '1992', xlab = 'Overall attitude', sub = 'mean = 55.1, variance = 652.6')
+mtext('(c)', 3, at = -46, padj = -4)
+
+par(mar = c(6, 3, 6, 3))
+
+hist(all9296$ft1993, main = '1993', xlab = 'Overall attitude', sub = 'mean = 58.8, variance = 699.8',  yaxt = 'n', ylab = '')
+hist(all9296$ft1994, main = '1994', xlab = 'Overall attitude', sub = 'mean = 55.5, variance = 810.9',  yaxt = 'n', ylab = '')
+
+par(mar = c(6, 2, 6, 4))
+
+hist(all9296$ft1996, main = '1996', xlab = 'Overall attitude', sub = 'mean = 59.8, variance = 925.3',  yaxt = 'n', ylab = '')
+
+dev.off()
